8e24ba8b06afbce8cad375aa8e9937c6de3b89bf
David Bae <baeddavid@gmail.com>
2019-09-12 19:50:47 -0500

    made pokemon cards clickable

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pokemon
 # Create your views here.
-
-class Pokemon:
-    def __init__(self, name, type, gender, description, level):
-        self.name = name
-        self.type = type
-        self.gender = gender
-        self.description = description
-        self.level = level
-
-pokemon = [
-    Pokemon('Pikachu', 'Electric', 'Make', 'Really bad', 54)
-]
 
 def home(request):
     return HttpResponse('<h1>Hello</h1>')
@@ -21,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def pokemon_index(request):
+    pokemon = Pokemon.objects.all()
     return render(request, 'pokemon/index.html', { 'pokemon': pokemon })
